47be0aea49d2d03dcd6e14da5a28169358009d65
ravigunz <ravigunz@gmail.com>
2023-05-14 23:46:46 +0530

    sx props add to Box

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Box } from "@mui/system";
+import { Typography } from "@mui/material";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
+      <Box
+        sx={{
+          mt: 40,
+          ml: "auto",
+          mr: "auto",
+          width: "500px",
+          bgcolor: "lightblue",
+          pt: 6,
+          pb: 4,
+          borderRadius: 0,
+          transition:
+            'border-radius 2s ease-in-out, transform 2s ease-in-out',
+          "&:hover": {
+            bgcolor: "#FF5733",
+            cursor: "pointer",
+            borderRadius: 100,
+            transform: "rotate(45deg)",
+          },
+        }}
+      >
+        <Typography variant="h2">Hi There!</Typography>
+      </Box>
     </div>
   );
 }
