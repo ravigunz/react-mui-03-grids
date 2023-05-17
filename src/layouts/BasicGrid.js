a40dcf8093f6c17456eec859e3a6524477cca2d5
ravigunz <ravigunz@gmail.com>
2023-05-17 14:28:28 +0530

    add fontSize for a Typography

--- src/layouts/BasicGrid.js
+++ src/layouts/BasicGrid.js
@@ -3,6 +3,7 @@ import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
+import { Typography } from '@mui/material';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: '#1A2027',
@@ -18,7 +19,9 @@ export default function BasicGrid() {
       <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
   {Array.from(Array(6)).map((_, index) => (
     <Grid item xs={2} sm={4} md={4} key={index}>
-      <Item>xs=2</Item>
+      <Item>
+        <Typography sx={{ fontSize: { xs: '20px', sm: '30px', md: '40px', lg: '100px' }}}>Hello</Typography>
+      </Item>
     </Grid>
   ))}
 </Grid>
