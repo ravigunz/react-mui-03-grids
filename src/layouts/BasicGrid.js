2eaff82066adfc8b5510fd947311dbaa23d71ebd
ravigunz <ravigunz@gmail.com>
2023-05-17 14:46:40 +0530

    complete Basic Grid

--- src/layouts/BasicGrid.js
+++ src/layouts/BasicGrid.js
@@ -16,11 +16,11 @@ const Item = styled(Paper)(({ theme }) => ({
 export default function BasicGrid() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+      <Grid container spacing={{ xs: 2, md: 3 }} direction='row'>
   {Array.from(Array(6)).map((_, index) => (
     <Grid item xs={2} sm={4} md={4} key={index}>
       <Item>
-        <Typography sx={{ fontSize: { xs: '20px', sm: '30px', md: '40px', lg: '100px' }}}>Hello</Typography>
+        <Typography sx={{ fontSize: { xs: '20px', sm: '30px', md: '40px', lg: '100px' }}}>Hello - {index + 1} </Typography>
       </Item>
     </Grid>
   ))}
